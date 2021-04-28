c7095c937ee28fa83b3792a3491b9096e7656a00
Brasen93 <dkrupinska@gmail.com>
2021-04-28 16:44:37 +0200

    fix 4

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const express = require('express')
 const config = require('config')
 const app = require('./src/app')
-const port = process.env.PORT || config.get('port')
+
+const port = process.env.PORT || config.get('feedmail.port')
 
 app.listen(port, () => console.log(`server is running at ${port}`))
--- src/app.js
+++ src/app.js
@@ -1,78 +1,37 @@
+/* eslint-disable quotes */
+/* eslint-disable no-useless-escape */
+
 const express = require('express')
-
-
 const config = require('config')
 const path = require('path')
 
-const fetch = require('node-fetch')
-
-const DB = require('./storage/db')
-const db = new DB()
-db.connect(config.db)
-
-const MailSender = require('./utils/mailSender')
+const RssParser = require('./utils/rssparser')
 const MailBuilder = require('./utils/mailbuilder')
-const FeedParser = require('./utils/fp2')
-const MailController = require('./controllers/MailController')
-const RssController = require('./controllers/RssController')
+const MailSender = require('./utils/mailsender')
 
-const rssRoutes = require('./routes/rssroutes')
+const Database = require('./storage/database')
+
+const UserController = require('./controllers/usercontroller')
+const MailController = require('./controllers/mailcontroller')
+
+const userRoutes = require('./routes/userroutes')
 const mailRoutes = require('./routes/mailroutes')
 
-const feedParser = new FeedParser()
-const mailSender = new MailSender()
-const mailBuilder = new MailBuilder()
-const mailController = new MailController(db,feedParser,mailSender,mailBuilder)
-const rssController = new RssController(db)
+const dbConfig = config.get('feedmail.db')
+const senderConfig = config.get('feedmail.mailgun')
+
+const db = new Database()
+db.connect(dbConfig)
+
+const userController = new UserController(db)
+const mailController = new MailController(db, new RssParser(), new MailBuilder(), new MailSender(senderConfig))
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, '../public')));
-app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, '../public')))
 app.use(express.json());
 
-// app.post('/send', async (req,res) => {
-//     try{
-//         console.log(req.body)
-//         await db.insert("feeds", req.body)
-//         res.status(200)
-//     } catch (e){
-//         console.error(e.message)
-//     }
-// })
-
-// app.get('/send', async (req,res) => {
-//     try{
-//         console.log('halo')
-//         await db.find("feeds", req.body.email)
-//         res.status(200).sendFile('./index.html')
-//     } catch (e){
-//         console.error(e.message)
-//     }
-
-// })
-
-
-// app.get('/api/v1/mail', async (req,res) => {
-//     try{
-//         const htmlContent = await mailController.build(req.query.email)
-//         res.send(htmlContent)
-//     }catch (e) {
-//         console.log(e)
-//         res.sendStatus(500)
-//     }
-// })
-
-// app.post('/api/v1/mail', async (req,res) => {
-//     try{
-//         await mailController.send(req.query.email)
-//         res.sendStatus(200)
-//     } catch (e) {
-//         console.log(e)
-//         res.sendStatus(500)
-//     }
-// })
-app.use('/v1', rssRoutes(rssController))
+app.use('/v1', userRoutes(userController))
 app.use('/v1', mailRoutes(mailController))
 
 module.exports = app
